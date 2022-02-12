32cc4b5c5d128ff56791d4c6ad2f44c0edcfd96c
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-02-12 15:50:08 +0300

    using different datastructure design and algorithms

--- DataStructure and Algorithms/GreedyForceStrategy.py
+++ DataStructure and Algorithms/GreedyForceStrategy.py
@@ -1,10 +1,20 @@
 import random
 from itertools import permutations
-import matplotlib.pyplot as plt
 from time import perf_counter
 from collections import Counter
 
-alltours = permutations
+from matplotlib import pyplot as plt
+
+
+def greedy_algorithm(cities, start=None):
+    C = start or first(cities)
+    tour = [C]
+    unvisited = set(cities - {C})
+    while unvisited:
+        C = nearest_neighbor(C, unvisited)
+        tour.append(C)
+        unvisited.remove(C)
+    return tour
 
 
 def distance_tour(aTour):
@@ -12,10 +22,14 @@ def distance_tour(aTour):
                for i in range(len(aTour)))
 
 
+def first(collection): return next(iter(collection))
+
+
+def nearest_neighbor(A, cities):
+    return min(cities, key=lambda C: distance_points(C, A))
+
+
 aCity = complex
-
-
-def distance_points(first, second): return abs(first - second)
 
 
 def generate_cities(number_of_cities):
@@ -28,12 +42,7 @@ def generate_cities(number_of_cities):
                      for c in range(number_of_cities))
 
 
-def brute_force(cities):
-    "Generate all possible tours of the cities and choose the shortest tour."
-    return shortest_tour(alltours(cities))
-
-
-def shortest_tour(tours): return min(tours, key=distance_tour)
+def distance_points(first, second): return abs(first - second)
 
 
 def visualize_tour(tour, style='bo-'):
@@ -63,13 +72,14 @@ def tsp(algorithm, cities):
     assert Counter(tour) == Counter(cities)  # Every city appears exactly once in tour
     visualize_tour(tour)
     print("{}: {} cities --> tour length {:.0f} (in {:.3f} sec)".format(name(algorithm), len(tour), distance_tour(tour),
+
                                                                         t1 - t0))
 
 
 def name(algorithm): return algorithm.__name__.replace('_tsp', '')
 
 
-tsp(brute_force, generate_cities(10))
+tsp(greedy_algorithm, generate_cities(2000))  # greedy_algorithm: 1988 cities --> tour length 15755 (in 0.497 sec)
+
+# a graph of the tour, .show() is good if you're using normal python session instead of magic functions as %inline
 plt.show()
-# output
-# brute_force: 10 cities --> tour length 1224 (in 15.637 sec)
